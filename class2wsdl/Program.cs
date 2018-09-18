01a3b49ee439cc892721fe6e38ef8739c0e1e88f
Gabriel Vidaurre Rodriguez <b47568@ecci.ucr.ac.cr>
2018-09-18 14:30:36 -0600

    Add project files.

---
+++ class2wsdl/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class2wsdl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String classStr = "";
+            if (args.Length == 1)
+            {
+                classStr = args[0];
+            }
+            else
+            {
+                Console.WriteLine("1 parameter is required\n" +
+                    "Usage: class2wsdl.exe class");
+                return;
+            }
+
+            new WSDLGenerator(classStr).Run();
+        }
+    }
+}
---
+++ class2wsdl/WSDLGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class2wsdl
+{
+    class WSDLGenerator
+    {
+        //https://docs.microsoft.com/en-us/dotnet/framework/reflection-and-codedom/reflection
+        Assembly classA;
+        Module[] classMods;
+        MethodInfo[] methods;
+        String wsdlStr;
+        
+        public WSDLGenerator(String classStr)
+        {
+            classA = Assembly.Load(classStr);
+            classMods = classA.GetModules();
+            wsdlStr = classA.GetName() + ".wsdl";
+        }
+
+        public void Run()
+        {
+            //no sé cuántos módulos hay...creo que es uno por clase?
+            foreach(Module mod in classMods)
+            {
+                methods.Concat(mod.GetMethods()); //extraer métodos
+            }
+            ///TODO Extraer parámetros de cada método
+            ///TODO Escribir en el archivo
+            WriteWSDL();
+        }
+
+        void WriteWSDL()
+        {
+            //opción 1: escribir línea por línea
+            //StreamWriter wsdlFile = new StreamWriter(wsdlStr);
+            //wsdlFile.WriteLine("<?xml version=\"1.0\"?>");
+
+            //opción 2: generar XML
+            //var xml = new XElement(...
+        }
+    }
+}
