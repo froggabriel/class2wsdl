32defdce94a95c6f2cea3ac4581307819f262327
Gabriel Vidaurre <vidaurre.gabriel@protonmail.ch>
2018-09-25 13:24:52 -0600

    Class1 + Person

---
+++ Examples/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1() { }
+    private double _number;
+    public double Number { get { return this._number; } set { this._number = value; } }
+    public void Clear() { this.DoClear(); }
+    private void DoClear() { this._number = 0; }
+    public double Add(double number) { return (this._number += number); }
+    public static double Pi { get { return Math.PI; } }
+    public static double GetPi() { return Pi; }
+    public int add(Person x, Person y) { return x.age + y.age; }
+}
---
+++ Examples/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+
+public class Person
+{
+    public int age { get { return age; } set { age = value; } }
+    public string name { get { return name; } set { name = value; } }
+    private double? height
+    {
+        get { return age; }
+        set { height = value; }
+    }
+    private double? weight
+    {
+        get { return age; }
+        set { weight = value; }
+    }
+
+    public Person()
+    {
+        age = 0;
+        name = "";
+        height = 0;
+        weight = 0;
+    }
+    public Person(int age, string name, double? height, double? weight)
+    {
+        this.age = age;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double getBMI()
+    {
+        if (height != null && weight != null)
+        {
+            return (double)weight / Math.Pow((double)height, 2);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
